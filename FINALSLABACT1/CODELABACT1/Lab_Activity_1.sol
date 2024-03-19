9fa1dce7687648826eb21bfa63f2cd0845c0642e
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2024-03-19 15:31:41 +0800

    Add files via upload

---
+++ FINALSLABACT1/CODELABACT1/Lab_Activity_1.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+    contract EtherWallet {
+        address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint256 _amount) public {
+        require(msg.sender == owner, "Only the owner can withdraw");
+        require(_amount <= address(this).balance, "Insufficient balance");
+        owner.transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+         return address(this).balance;
+    }
+}
+
+
+//Ryan Joshua F. Milan
+//Comment
+//The EtherWallet contract allows users to securely deposit and withdraw Ether, with exclusive withdrawal rights reserved for the designated owner. 
+//Its transparent balance tracking function ensures openness about the contract's financial state.
